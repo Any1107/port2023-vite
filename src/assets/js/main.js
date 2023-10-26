84b670dc37975175dd69f00f4c3161bbba621039
Your Name <you@example.com>
2023-10-26 11:25:54 +0900

    레이아웃 완성

---
+++ src/assets/js/main.js
@@ -0,0 +1,20 @@
+import { menu } from "./menu.js";
+import { link } from "./link.js";
+import { port } from "./port.js";
+import { smooth } from "./smooth.js";
+
+window.addEventListener("load",function(){
+    menu();
+});
+
+window.addEventListener("load",function(){
+    link();
+});
+
+window.addEventListener("load",function(){
+    port();
+});
+
+window.addEventListener("load",function(){
+    smooth();
+});
